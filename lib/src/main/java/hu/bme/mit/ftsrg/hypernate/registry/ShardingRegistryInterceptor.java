0b6be83edd57e09f5aec6753055760d9ea219799
Kuczó Péter <144066539+somerandomCSstudent@users.noreply.github.com>
2026-04-29 22:56:06 +0200

    ShardingRegistryInterceptor

---
+++ lib/src/main/java/hu/bme/mit/ftsrg/hypernate/registry/ShardingRegistryInterceptor.java
@@ -0,0 +1,20 @@
+package hu.bme.mit.ftsrg.hypernate.registry;
+
+public class ShardingRegistryInterceptor implements RegistryInterceptor {
+
+    @Override
+    public void mustCreate() {
+
+    }
+
+    @Override
+    public void mustRead() {
+
+    }
+
+    @Override
+    public void mustWrite() {
+
+    }
+
+}
